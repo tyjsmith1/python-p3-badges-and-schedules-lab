6ff5f10150b2a33f0cd46b86d80a8c3136a40c2f
tylersmith <tyjsmith1@gmail.com>
2023-12-11 14:51:46 -0600

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    message = f"Hello, my name is {name}."
+    return message
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = f"Hello, my name is {name}."
+        badges.append(badge)
+    return badges
 
 def assign_rooms(names):
-    return None
+    assignments = []
+    room_number = 1
+    for name in names:
+        assignment = f"Hello, {name}! You'll be assigned to room {room_number}!"
+        assignments.append(assignment)
+        room_number += 1
+    return assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    
+    assignments = assign_rooms(names)
+    for assignment in assignments:
+        print(assignment)
